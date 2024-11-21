e3d940f494879c49a348263f31e5ec0e7f4f008d
tahaenesc <141641915+tahaenesc@users.noreply.github.com>
2024-11-22 02:01:46 +0300

    interview

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,35 @@
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+
+async function page() {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+  const userSettings = await prisma.userSettings.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (!userSettings) {
+    redirect("/wizard");
+  }
+
+  return (
+    <div className="h-full bg-background">
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <p className="text-3xl font-bold">Hello, {user.firstName}! 👋 </p>
+          <div className="flex">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
